080b021048b6d7956b69341425894f0beddc8d71
dikaarnnd <dika4kiga@gmail.com>
2024-10-31 16:29:11 +0700

    3

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,26 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
   return (
-    <main>homepage</main>
-  );
+    <section>
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row justify-between items-center xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Web Developer</span>
+            <h1 className="h1 mb-6">
+              Hello, I'm <br /> <span className="text-accent">Dika Arnanda Putra</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">I am skilled in a wide variety of programming languages, and excel in interactive and responsive web building experience.
+            </p>
+          </div>
+          {/* photo */}
+          <div>foto</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home;
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -10,12 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-accent text-primary hover:bg-accent-hover",
+        primary: "bg-primary text-white",
+        outline: "border border-accent bg-transparent text-accent hover:bg-accent hover:text-primary"
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "h-[44px] px-6",
+        md: "h-[48px] px-6",
+        lg: "h-[56px] px-8 text-sm tracking-[2px]",
       },
     },
     defaultVariants: {
